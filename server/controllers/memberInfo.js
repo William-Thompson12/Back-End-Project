3e9ca0c4bde30dc16b8af5296cba3f1875f5e0c0
William Thompson <68480511+Frosty1212@users.noreply.github.com>
2020-11-22 10:49:41 -0500

    server starts, routes and controllers bugged

---
+++ server/controllers/memberInfo.js
@@ -0,0 +1,38 @@
+// Global variables for our prototype
+// Orders array for storing and manipulating orders
+var memberInfo = [];
+// Used to assign unique ids to each order
+var primaryId = 1;
+
+exports.findAll = (req, res,) => {
+    if (!memberInfo.length) {
+        next();
+    }
+    res.send(memberInfo);
+}
+
+exports.create = (req, res) => {
+    // req.body now represents the actual request body
+    // as a javascript object
+    memberInfo.push({
+        id: primaryId,
+        customer_name: `${req.body.firstname + ' ' + req.body.lastname}`,
+        address: req.body.address,
+        city: req.body.city,
+        state: req.body.state,
+        zip: req.body.zip
+    });
+    primaryId++;
+
+    res.status(200).json({
+        message: "Order created succesfully"
+    })
+}
+
+exports.delete = (req, res) => {
+    res.send('delete members');
+}
+
+exports.findOne = (req, res) => {
+   res.send('members by id');
+}
--- server/index.js
+++ server/index.js
@@ -1,30 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3001;
 
 // Importing our Order Routes
-const memberFormRoutes = require('/routes/memberForm');
-const memberRoutes = require('/routes/member');
+const memberRoutes = require('./routes/memberInfo');
+
+// Middleware
+
+// This is going to capture the request body (req.body), and 
+// run it through JSON.parse(), returning an object that will 
+// be available as req.body
 
 app.use(express.json());
 // request body has been parsed
 app.use(express.urlencoded({extended: false}));
 // request body has been url encoded
 
-// Custom Middleware for visual
+// Static Files
+app.use(express.static('public')); // no match found? next()
+
+// Custom Middleware
 app.use((req, res, next) => {
-  console.log(`Request received: ${req.method} ${req.path}`) 
-  next();
+    console.log(`Request received: ${req.method} ${req.path}`) 
+    next();
 })
 
-// Routes - Orders
-app.use('/memberForm', memberFormRoutes);
-app.use('/member', memberRoutes);
+// Routes - memberInfo
+app.use('/memberInfo', memberRoutes);
 
 app.get('*', (req, res) => {
     res.status(404).send('Not found');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log('server started');
 })
+
