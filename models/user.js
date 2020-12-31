bc6e1e18ae46dd18162cab59ba03cdc48eac7c3c
nastrodamous <andre@Prongs.localdomain>
2020-12-31 11:01:10 -0500

    updated routes

--- models/user.js
+++ models/user.js
@@ -17,6 +17,7 @@ module.exports = (sequelize, DataTypes) => {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
+    password: DataTypes.STRING,
     gender: DataTypes.STRING,
     preference: DataTypes.STRING,
     city: DataTypes.STRING,
